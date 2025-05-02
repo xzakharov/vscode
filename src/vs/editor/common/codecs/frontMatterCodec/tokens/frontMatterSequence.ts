fec0c0b2df41291c28617b38db42e89247c599ea
Oleg Solomko <legomushroom@gmail.com>
2025-05-01 17:16:52 -0700

    support generic token sequences inside front matter arrays

--- src/vs/editor/common/codecs/frontMatterCodec/tokens/frontMatterSequence.ts
+++ src/vs/editor/common/codecs/frontMatterCodec/tokens/frontMatterSequence.ts
@@ -5,7 +5,15 @@
 
 import { BaseToken } from '../../baseToken.js';
 import { FrontMatterValueToken } from './frontMatterToken.js';
-import { TSimpleDecoderToken } from '../../simpleCodec/simpleDecoder.js';
+import { type TSimpleDecoderToken } from '../../simpleCodec/simpleDecoder.js';
+import { Space, Tab, VerticalTab, Word } from '../../simpleCodec/tokens/index.js';
+
+/**
+ * TODO: @legomushroom
+ */
+export const VALID_SPACE_TOKENS = Object.freeze([
+	Space, Tab, VerticalTab,
+]);
 
 /**
  * Token represents a generic sequence of tokens in a Front Matter header.
@@ -22,15 +30,71 @@ export class FrontMatterSequence extends FrontMatterValueToken<string> {
 	/**
 	 * TODO: @legomushroom
 	 */
+	public override get tokens(): readonly TSimpleDecoderToken[] {
+		return this.currentTokens;
+	}
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	private readonly currentTokens: TSimpleDecoderToken[];
+
+	constructor(
+		tokens: readonly TSimpleDecoderToken[],
+	) {
+		super(BaseToken.fullRange(tokens));
+
+		this.currentTokens = [...tokens];
+	}
+
+	/**
+	 * TODO: @legomushroom
+	 */
 	// TODO: @legomushroom - trim spaces?
 	public get cleanText(): string {
 		return BaseToken.render(this.tokens);
 	}
 
-	constructor(
-		public override readonly tokens: readonly TSimpleDecoderToken[],
-	) {
-		super(BaseToken.fullRange(tokens));
+	/**
+	 * TODO: @legomushroom
+	 */
+	// TODO: @legomushroom - unit test
+	// TODO: @legomushroom - use TSpaceToken[] for the return value instead
+	public trimEnd(): readonly TSimpleDecoderToken[] {
+		let index = this.currentTokens.length - 1;
+		while (index >= 0) {
+			const token = this.currentTokens[index];
+
+			if (token instanceof Space || token instanceof Tab || token instanceof VerticalTab) {
+				index--;
+
+				continue;
+			}
+
+			break;
+		}
+
+		const trimmedTokens = this.currentTokens.splice(index + 1);
+
+		// TODO: @legomushroom
+		if (this.currentTokens.length === 0) {
+			this.collapseRangeToStart();
+
+			this.currentTokens.push(
+				Word.newOnLine(
+					'',
+					this.range.startLineNumber,
+					this.range.startColumn,
+				),
+			);
+		}
+
+		// TODO: @legomushroom
+		this.withRange(
+			BaseToken.fullRange(this.tokens),
+		);
+
+		return trimmedTokens;
 	}
 
 	public override toString(): string {
