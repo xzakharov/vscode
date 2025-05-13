aba35e223152d777a9a13fbe4554706c53516b87
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-05-13 10:50:33 -0700

    tidy up

--- src/vs/workbench/contrib/extensions/browser/extensionsWorkbenchService.ts
+++ src/vs/workbench/contrib/extensions/browser/extensionsWorkbenchService.ts
@@ -681,8 +681,6 @@ class Extensions extends Disposable {
 				this._onChange.fire({ extension });
 			}
 		}
-
-		// Flag extensions that
 		if (flagMissingFromGallery) {
 			for (const extension of missingFromGalleryExtensions) {
 				extension.missingFromGallery = true;
@@ -735,7 +733,6 @@ class Extensions extends Disposable {
 					continue;
 				}
 			}
-			// If we got to here but nothing in the gallery, assume it's been pulled.
 			if (installed.local?.source === 'gallery') {
 				missing.push(installed);
 			}
@@ -1283,10 +1280,6 @@ export class ExtensionsWorkbenchService extends Disposable implements IExtension
 
 	get outdated(): IExtension[] {
 		return this.installed.filter(e => e.outdated && e.local && e.state === ExtensionState.Installed);
-	}
-
-	get missingFromGallery(): IExtension[] {
-		return this.installed.filter(e => e.missingFromGallery);
 	}
 
 	async queryLocal(server?: IExtensionManagementServer): Promise<IExtension[]> {
